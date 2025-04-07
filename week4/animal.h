0dd2c2b089effac2c8d70d97d4e7f3d3913b6076
rkalsdud <rkalsdud0707@gmail.com>
2025-04-07 15:26:21 +0900

    실습1

---
+++ week4/animal.h
@@ -0,0 +1,7 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+#include <stdio.h>
+void print_dog(void);
+void print_blackcow(void);
+void print_turtle(void);
+#endif
---
+++ week4/main.c
@@ -0,0 +1,7 @@
+#include "animal.h"
+#include <stdio.h>
+void main(void){
+	print_dog();
+	print_blackcow();
+	print_turtle();
+}
